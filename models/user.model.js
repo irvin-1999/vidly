fee723a9ebcbd394f4614c1075fc50a47091f46b
Boitumelo Iven Moshitwa <itumelo553@gmail.com>
2024-02-21 12:25:56 +0200

    add user model

---
+++ models/user.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 25,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 1024,
+    unique: true,
+  },
+});
+
+const User = mongoose.model("user", genreSchema);
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  };
+
+  return Joi.validate(user, schema);
+}
+
+module.exports = {
+  User,
+  validateUser,
+};
