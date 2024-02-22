483da45cc944d738cc976d943f283122de677375
Boitumelo Iven Moshitwa <itumelo553@gmail.com>
2024-02-22 15:45:35 +0200

    odularize code in the index to separate concerns

--- index.js
+++ index.js
@@ -1,60 +1,14 @@
-require("express-async-errors");
 const winston = require("winston");
-require("winston-mongodb");
-const error = require("./middleware/error");
-const config = require("config");
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
-
-const mongoose = require("mongoose");
-
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/user");
-const auth = require("./routes/auth");
 const express = require("express");
 const app = express();
-
-process.on("uncaughtException", (ex) => {
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
-process.on("unhandledRejection", () => {
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
-winston.add(winston.transports.File, { filename: "logfile.log" });
-winston.add(winston.transports.MongoDB, {
-  db: "mongodb://localhost/vidly",
-  level: "error",
-});
-
-if (!config.has("jwtPrivateKey")) {
-  console.log("FATAL ERROR: jwtPrivateKey is not defined");
-  process.exit(1);
-}
-
-mongoose
-  .connect("mongodb://localhost:27017/vidly")
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Couldn't connect to MongoDb", err));
+require("./startup/logging");
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/confiq")();
+require("./startup/validation")();
 
 const PORT = 3000;
 
-app.use(express.json());
-app.use("/genres", genres);
-app.use("/customers", customers);
-app.use("/movies", movies);
-app.use("/rentals", rentals);
-app.use("/users", users);
-app.use("/auth", auth);
-
-app.use(error);
-
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  winston.info(`Listening on port ${PORT}`);
 });
---
+++ startup/routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const genres = require("../routes/genres");
+const customers = require("../routes/customers");
+const movies = require("../routes/movies");
+const rentals = require("../routes/rentals");
+const users = require("../routes/user");
+const auth = require("../routes/auth");
+const error = require("../middleware/error");
+
+module.exports = function (app) {
+  app.use(express.json());
+  app.use("/genres", genres);
+  app.use("/customers", customers);
+  app.use("/movies", movies);
+  app.use("/rentals", rentals);
+  app.use("/users", users);
+  app.use("/auth", auth);
+  app.use(error);
+};
