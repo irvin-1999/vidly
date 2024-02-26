66e19501bbc3e39a68fe617de45c3537fb14cae5
Boitumelo Iven Moshitwa <itumelo553@gmail.com>
2024-02-26 13:29:39 +0200

    add test case for dynamic genre route

--- tests/intergration/genres.test.js
+++ tests/intergration/genres.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+const { Genre } = require("../../models/genres.model");
 
 let server;
 
@@ -6,15 +7,32 @@ describe("/Genres", () => {
   beforeEach(() => {
     server = require("../../index");
   });
-  // afterEach(() => {
-  //   server.close();
-  // });
+  afterEach(async () => {
+    server.close();
+    await Genre.removeAllListeners({});
+  });
 
   describe("GET /", () => {
     test("Should return all genres", async () => {
+      // await Genre.collection.insertMany([
+      //   { name: "genre1" },
+      //   { name: "genre2" },
+      // ]);
       const res = await request(server).get("/genres");
 
       expect(res.status).toBe(200);
+      // expect(res.body.length).toBe(2);
+      // expect(res.body.some((g) => g.name === "genre1")).toBeTruthy();
+      // expect(res.body.some((g) => g.name === "genre2")).toBeTruthy();
+    });
+  });
+  describe("GET /id", () => {
+    test("should return genre if a valid id is passed", async () => {
+      const genre = new Genre({ name: "genre1" });
+      await genre.save();
+      const res = await request(server).get("/genres/" + genre._id);
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("name", "genre1");
     });
   });
 });
