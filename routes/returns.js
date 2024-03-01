b875c11f4cfe2a997389056186153951c52c4b0d
Boitumelo Iven Moshitwa <itumelo553@gmail.com>
2024-03-01 17:52:14 +0200

    refactor the tests and the code in the rental model

--- routes/returns.js
+++ routes/returns.js
@@ -1,29 +1,28 @@
-const moment = require("moment");
+const Joi = require("joi");
+const validate = require("../middleware/validate");
 const { Rental } = require("../models/rental.model");
 const { Movie } = require("../models/movies.model");
 const auth = require("../middleware/auth");
 const express = require("express");
-const mongoose = require("mongoose");
 const router = express.Router();
 
-router.post("/returns", auth, async (req, res, next) => {
-  if (!req.body.customerId)
-    return res.status(400).send("CustmoerId not provided");
+function validateReturns(req) {
+  const schema = {
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required(),
+  };
 
-  if (!req.body.movieId) return res.status(400).send("Movie not provided");
+  return Joi.validate(req, schema);
+}
 
-  const rental = Rental.findOne({
-    "customer._id": req.body.customerId,
-    "movie._id": req.body.movieId,
-  });
+router.post("/", [auth, validate(validateReturns)], async (req, res, next) => {
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
 
   if (!rental) return res.status(404).send("Rental Not found");
   if (rental.dateReturned)
     return res.status(400).send("Rental Already Processed");
 
-  rental.dateReturned = new Date();
-  const rentalDays = moment().diff(rental.dateOut, "days");
-  rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
+  rental.return();
 
   await rental.save();
 
